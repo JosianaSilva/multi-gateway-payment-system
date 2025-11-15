9d8fa1c5f7bfcc6ea9a943bfba8da10b656355dc
Josiana Silva <josianasilva655@gmail.com>
2025-11-15 12:14:26 -0300

    feat: inicializa projeto

---
+++ start/routes.ts
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+// Product routes
+router
+  .group(() => {
+    router.get('/products', '#controllers/products_controller.index')
+    router.post('/products', '#controllers/products_controller.store')
+    router.get('/products/:id', '#controllers/products_controller.show')
+    router.put('/products/:id', '#controllers/products_controller.update')
+    router.patch('/products/:id', '#controllers/products_controller.update')
+    router.delete('/products/:id', '#controllers/products_controller.destroy')
+    router.patch('/products/:id/toggle-active', '#controllers/products_controller.toggleActive')
+  })
+  .prefix('/api/v1')
